40f757f373b6dd74bd6df6c2fe447da0e9c291cb
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-03 20:31:21 +0300

    hw34

--- js/hw-3.js
+++ js/hw-3.js
@@ -9,7 +9,7 @@ if (password === userPass) {
 
 //task 2
 let c = Number(prompt('Введите число'));
-if (с > 0 && с < 10) {
+if (c > 0 && c < 10) {
     console.log('Верно');
 } else {
     console.log('Неверно');
