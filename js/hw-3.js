2bd9a063b076548e50ef5746f700c7e940fad926
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-03 20:36:59 +0300

    hw35

--- js/hw-3.js
+++ js/hw-3.js
@@ -16,6 +16,13 @@ if (c > 0 && c < 10) {
 }
 
 //task 3
+let d = Number(prompt('Введите первое число');
+let e = Number(prompt('Введите второе число');
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
 
 //task 4
 
