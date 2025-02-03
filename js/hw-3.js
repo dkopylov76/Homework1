ac180f7f554efad79f0a14cd2385062e965c122b
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-03 20:20:28 +0300

    hw32

--- js/hw-3.js
+++ js/hw-3.js
@@ -1,7 +1,7 @@
 let password = 'passw';
 let userPass = prompt('Введите пароль');
 if (password === userPass) {
-    console.log('Пароль введен верно');
+    console.log("Пароль введен верно");
 } else {
-    console.log('Пароль введен неправильно');
+    console.log("Пароль введен неправильно");
 }
