5e5e723b7afc2aaae7af67a2eab02cd986ece4d5
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-04 22:12:56 +0300

    correct_hw_js1

--- js/hw-3.js
+++ js/hw-3.js
@@ -33,16 +33,24 @@ alert(Number(a) + Number(b));
 //task 5
 let monthNumber = Number(prompt("Введите номер месяца"));
 switch (monthNumber) {
-    case (1 || 2 || 12):
+    case 1:
+    case 2:
+    case 12:
         console.log('Зима');
     break;
-    case (3 || 4 || 5):
+    case 3:
+    case 4:
+    case 5:
         console.log('Весна');
     break;
-    case (6 || 7 || 8):
+    case 6:
+    case 7:
+    case 8:
         console.log('Лето');
     break;
-    case (9 || 10 || 11):
+    case 9:
+    case 10:
+    case 11:
         console.log('Осень');
     break;
     default:
