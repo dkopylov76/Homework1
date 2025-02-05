0b0a5f8a536f66e85ca5977de3ed9f274e2de4c4
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-05 21:45:37 +0300

    hw_js_420

--- js/hw-4.js
+++ js/hw-4.js
@@ -46,7 +46,7 @@ console.log(num);
 //task 6
 let friday = prompt('Введите число первой пятницы месяца (от 1 до 7)');
 for (let m = 1; m <= 31; m++) {
-    if ((m = friday) || (m = friday+7) || (m = friday+14) || (m = friday+21) || (m = friday+28)) {
+    if (m = friday) {
         break;
     }
     console.log(`Сегодня пятница, ${m}-е число. Необходимо подготовить отчет.`);
