0d57bf6ebbcbecda52603f433513ce05f380b107
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-05 20:37:43 +0300

    hw_js_44

--- js/hw-4.js
+++ js/hw-4.js
@@ -1,6 +1,13 @@
 //task 1
 let i = 0;
+while (i < 2) {
+    console.log('Привет!');
+    i++;
+}
+
+//task 2
+let i = 0;
 while (i < 5) {
-    console.log('Привет!')
+    console.log(i+1);
     i++;
 }
