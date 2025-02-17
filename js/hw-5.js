5ee018d53b907f209e76c3012a3a811779772740
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-17 22:15:56 +0300

    hw5_3

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,14 +1,15 @@
-const hiddenNumber = Math.floor(Math.random() * 100) + 1;
-///console.log(hiddenNumber);
-alert('Попробуйте угадать число от 1 до 100.');
-while(true) {
-    let userNumber = Number(prompt('Ваш вариант?'));
-    if (userNumber === hiddenNumber) {
-        alert(`Вы угадали число ${hiddenNumber}!`);
-        break;
-    } else if (userNumber < hiddenNumber) {
-        alert('Загаданное число больше. Попробуйте еще раз.')
-    } else {
-        alert('Загаданное число меньше. Попробуйте еще раз.')
+function gameHiddenNumber() {
+    const hiddenNumber = Math.floor(Math.random() * 100) + 1;
+    alert('Попробуйте угадать число от 1 до 100.');
+    while(true) {
+        let userNumber = Number(prompt('Ваш вариант?'));
+        if (userNumber === hiddenNumber) {
+            alert(`Вы угадали число ${hiddenNumber}!`);
+            break;
+        } else if (userNumber < hiddenNumber) {
+            alert('Загаданное число больше. Попробуйте еще раз.')
+        } else {
+            alert('Загаданное число меньше. Попробуйте еще раз.')
+        }
     }
 }
