d898674431d608bebe8e4a64039368e0b0a561be
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-17 22:36:38 +0300

    hw_js_5_1

---
+++ js/hw-5.js
@@ -0,0 +1,12 @@
+function defineMoreLess() {
+    alert('Введите два числа.');
+    let firstNumber = Number(prompt('Первое число?'));
+    let secondNumber = Number(prompt('Второе число?'));
+    if (firstNumber < secondNumber) {
+        alert(`Меньшее число ${firstNumber}.`);
+    } else if (firstNumber > secondNumber) {
+        alert(`Меньшее число ${secondNumber}.`);
+    } else {
+        alert('Числа равны.');
+    }
+}
