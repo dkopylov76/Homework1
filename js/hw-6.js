e389dd49538ba24f4550ef4ffb88f66274700065
dkopylov76 <d.kopylov.76@gmail.com>
2025-02-25 23:05:17 +0300

    hw_6_html_5

--- js/hw-6.js
+++ js/hw-6.js
@@ -1,4 +1,4 @@
-const firstNumber = Math.floor(Math.random() * 100) + 1;
+/*const firstNumber = Math.floor(Math.random() * 100) + 1;
 const secondNumber = Math.floor(Math.random() * 100) + 1;
 const mathAction = ['+', '-', '*', '/'];
 result = join(firstNumber, mathAction, secondNumber);
@@ -11,3 +11,13 @@ while(true) {
         console.log('Ошибка.')
     }
 }
+*/
+
+const firstNumber = [];
+const N = 100;
+ 
+for (let i = 0; i < N; i++) {
+    firstNumber.push(i);
+}
+
+console.log(firstNumber);
