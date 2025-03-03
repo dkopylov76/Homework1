b49986e5e0bcc7ad545e6ac28b4f795d3740df96
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-03 11:43:38 +0300

    hw_6_js_7

--- js/hw-6.js
+++ js/hw-6.js
@@ -1,5 +1,6 @@
 //task 1
 const numbers = [1, 5, 4, 10, 0, 3];
+
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
     if (numbers[i] == 10) break;
@@ -7,12 +8,26 @@ for (let i = 0; i < numbers.length; i++) {
 
 //task 2
 const numbs = [1, 5, 4, 10, 0, 3];
+
 for (let i = 0; i < numbs.length; i++) {
     if (numbs[i] === 4)
     console.log(i);
+    break;
 };
 
 //task 3
 const nums = [1, 3, 5, 10, 20];
-let joinNums = nums.join(' ');
-console.log(joinNums);
+console.log(nums.join(' '));
+
+//task 4
+const array1 = [];
+
+for (i = 0; i < 3; i++) {
+    const array2 = [];
+    for (i = 0; i < 3; i++) {
+        array2.push(1);
+    }
+    array1.push(array2);
+}
+
+console.log(array1);
