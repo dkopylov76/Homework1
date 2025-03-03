4eaf8df5ba8586851aad73836e7f5ff1ee572fbb
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-03 11:29:23 +0300

    hw_6_js_5

--- js/hw-6.js
+++ js/hw-6.js
@@ -7,7 +7,7 @@ for (let i = 0; i < numbers.length; i++) {
 
 //task 2
 const numbs = [1, 5, 4, 10, 0, 3];
-numbs.forEach((i, index) => {
+for (let i = 0; i < numbs.length; i++) {
     if (numbs[i] === 4)
-    console.log(`${index}: ${i}`);
-});
+    console.log(i);
+};
