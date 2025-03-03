051680bd3fc563e7329195b6f4cd3b861be3fcd7
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-03 19:14:00 +0300

    hw_6_html_9

--- js/miniGames.js
+++ js/miniGames.js
@@ -20,12 +20,35 @@ function gameHiddenNumber() {
 function gameSimpleMath() {
     const firstNumber = Math.floor(Math.random() * 100) + 1;
     const secondNumber = Math.floor(Math.random() * 100) + 1;
-    const mathActions = ['+', '-', '*', '/'];
+    const mathActions = ['+', '-'];
     const mathAction = mathActions[Math.floor(Math.random() * mathActions.length)];
+    let result;
 
     console.log(firstNumber);
     console.log(secondNumber);
     console.log(mathAction);
+
+    switch (mathAction) {
+        case '+':
+            userAnswer = +prompt(`Сложите ${firstNumber} и ${secondNumber}. Ваш ответ?`);
+            result = firstNumber + secondNumber;
+            if (userAnswer === result) {
+                alert('Верно!');
+                break;
+            } else {
+                alert('Ошибка!')
+            }
+        case '-':
+            userAnswer = +prompt(`Найдите разность ${firstNumber} и ${secondNumber}. Ваш ответ?`);
+            result = Math.abs(firstNumber - secondNumber);
+            if (userAnswer === result) {
+                alert('Верно!');
+                break;
+            } else {
+                alert('Ошибка!')
+            }
+        }
+    }
     
     /*alert('Попробуй решить простые арифметические задачи.');
     while(true) {
@@ -36,4 +59,3 @@ function gameSimpleMath() {
             console.log('Ошибка.')
         }
     }*/
-}
