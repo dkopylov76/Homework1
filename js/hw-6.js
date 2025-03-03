1b30ecca7cc33b425f566cb8985a4df2e342a013
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-03 11:20:26 +0300

    hw_6_js_3

--- js/hw-6.js
+++ js/hw-6.js
@@ -1,6 +1,6 @@
 //task 1
 const numbers = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
     if (numbers[i] == 10) break;
-    console.log(numbers[i+1]);
 }
