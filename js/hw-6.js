bc8435ad0e57b291f57ddbcb5e267cc4bb793501
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-03 11:46:03 +0300

    hw_6_js_8

--- js/hw-6.js
+++ js/hw-6.js
@@ -8,11 +8,9 @@ for (let i = 0; i < numbers.length; i++) {
 
 //task 2
 const numbs = [1, 5, 4, 10, 0, 3];
-
 for (let i = 0; i < numbs.length; i++) {
-    if (numbs[i] === 4)
+    if (numbs[i] == 4)
     console.log(i);
-    break;
 };
 
 //task 3
@@ -22,9 +20,9 @@ console.log(nums.join(' '));
 //task 4
 const array1 = [];
 
-for (i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
     const array2 = [];
-    for (i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         array2.push(1);
     }
     array1.push(array2);
