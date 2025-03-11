295b8960ffa210cb0ddb89375d84bb37b5786bd9
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-11 19:40:25 +0300

    hw_7_js_3

--- js/hw-7.js
+++ js/hw-7.js
@@ -1,3 +1,12 @@
 //task 1
 let str = 'js';
 console.log(str.toUpperCase('js'));
+
+//task 2
+const words = ['Вниз','вверх','внутрь','Наружу','Внезапно'];
+const search = 'вн';
+words.forEach((word) => {
+    if (word.toLowerCase().includes(search.toLowerCase())) {
+        console.log(word);
+    }
+});
