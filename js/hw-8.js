5b3f3753b56f2dee2ae33c2a44020909d7e0ad79
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-19 23:08:53 +0300

    hw_8_js_4

--- js/hw-8.js
+++ js/hw-8.js
@@ -7,3 +7,27 @@ const people = [
 ];
 
 console.log(people.sort((a, b) => a.age - b.age));
+
+//task 2
+function isPositive(number) {
+    return number > 0;
+}
+/*function isMale() {
+    // писать код тут
+}
+ 
+function filter() {
+    const filteredNumbers = numbers.filter(number => number > 10)
+}
+*/    
+console.log(filter([3, -4, 1, 9], isPositive));
+/*    
+const people1 = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+];
+    
+console.log(filter(people1, isMale));
+*/
