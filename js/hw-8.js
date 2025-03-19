d0c7e0ae1c0c92958face499b9cfcc369a485b99
dkopylov76 <d.kopylov.76@gmail.com>
2025-03-19 21:55:11 +0300

    hw_8_js_1

---
+++ js/hw-8.js
@@ -0,0 +1,9 @@
+//task 1
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+];
+
+people.sort((a, b) => a.age - b.age);
